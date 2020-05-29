7075e437b5ba9552b7b28f956ff774fcc9b5405d
Filip Chalupa <chalupa.filip@gmail.com>
2020-05-29 16:13:21 +0200

    Multiple counters

--- index.js
+++ index.js
@@ -9,17 +9,22 @@ app.set('view engine', 'handlebars');
 
 const port = parseInt(process.env.PORT || '', 10) || 8080;
 
-let visitCounter = 0;
+const visitCounter = {
+  home: 0,
+  contacts: 0,
+};
 
 // app.get('/', (req, res) => res.send(`Toto je ${++visitCounter}. návštěva.`));
 
 app.get('/', (req, res) => {
+  visitCounter.home += 1;
   res.render('home', {
-    visitCounter: ++visitCounter,
+    visitCounter,
   });
 });
 
 app.get('/contacts', (req, res) => {
+  visitCounter.contacts += 1;
   res.render('contacts');
 });
 
