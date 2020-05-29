b3c42cf3b85716321e1c77a95ef2ab3210b121d3
Filip Chalupa <chalupa.filip@gmail.com>
2020-05-29 16:05:23 +0200

    Handlebars

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+
 
 const app = express();
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+
 const port = parseInt(process.env.PORT || '', 10) || 8080;
 
 let visitCounter = 0;
 
-app.get('/', (req, res) => res.send(`Toto je ${++visitCounter}. návštěva.`));
+// app.get('/', (req, res) => res.send(`Toto je ${++visitCounter}. návštěva.`));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.get('/', (req, res) => {
+  res.render('home', {
+    visitCounter: ++visitCounter,
+  });
+});
+
+app.get('/contacts', (req, res) => {
+  res.render('contacts');
+});
+
+app.listen(port);
